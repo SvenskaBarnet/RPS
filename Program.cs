60bdbee2557ae1ea2643b6d50666fe75bd1ff529
= <klas_hermansson@hotmail.com>
2023-10-26 15:30:36 +0200

    Part two finished

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] data = File.ReadAllLines("data.txt");
+string[] strategy = { "A X", "A Y", "A Z", "B X", "B Y", "B Z", "C X", "C Y", "C Z" };
+int[] points = { 3, 4, 8, 1, 5, 9, 2, 6, 7, };
+int sum = 0;
+
+foreach (string line in data)
+{
+	for (int i = 0; i < strategy.Length; i++)
+	{
+		if (line.Equals(strategy[i]))
+		{
+			sum += points[i];
+		}
+	}
+}
+
+Console.WriteLine(sum);
